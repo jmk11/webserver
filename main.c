03356e0c40117fd1db54fad2a41f24ddac57f7a1
jmk11 <z5160822@student.unsw.edu.au>
2020-04-05 04:01:33 +1000

    Implement getfilename()

--- main.c
+++ main.c
@@ -18,13 +18,13 @@ int buildSocket(unsigned short port);
 int dropPermissions(unsigned short goaluid);
 int handleRequest(int clientfd, char *requestbuf, ssize_t requestLength);
 
-int getFileName(const char *request, char **filename);
+int getFileName(char *request, char **filename);
 int sanitiseRequest(char *filename);
 int loadRequestedFile(const char *filename, char **filebuf, off_t *fileLength);
 int loadHTTPHeaders(off_t fileLength, char **headersbuf, unsigned long *headersLength);
 
-int fileNotFound();
-int fileNotAvailable();
+int fileNotFound(int clientfd);
+int fileNotAvailable(int clientfd);
 
 int main(int argc, char **argv)
 {
@@ -64,7 +64,7 @@ int main(int argc, char **argv)
 									"Content-Type: text/html; charset=utf-8\r\n\r\n"
 									"<html><title>Justin</title><body><h3><center>Under Construction</br></br></br></br></br>Coming Christmas 2002!</h3></center></body></html>";
 			
-			requestLen = recv(clientfd, requestbuf, BUFSIZ, 0);
+			requestLen = recv(clientfd, requestbuf, BUFSIZ-1, 0);
 			if (requestLen < 0) {
 				perror("Error recv");
 			}
@@ -92,25 +92,27 @@ int handleRequest(int clientfd, char *requestbuf, ssize_t requestLength)
 	int res;
 	
 	res = getFileName(requestbuf, &filename);
-	if (res != 0) { 
+	if (res != 0) {
 		fileNotFound(clientfd);
 		// end this thread
 		return 1;
 	}
+	printf("Filename: %s", filename);
+	
 	res = sanitiseRequest(filename);
 	if (res != 0) {
 		fileNotFound(clientfd);
-		free(filename);
+		//free(filename);
 		return 1;
 	}
 	res = loadRequestedFile(filename, &filebuf, &fileLength);
 	if (res != 0) {
-		fileNotFound();
-		free(filename);
+		fileNotFound(clientfd);
+		//free(filename);
 		return 1;
 	}
 	// won't scale with large file size
-	free(filename);
+	//free(filename);
 	
 	res = loadHTTPHeaders(fileLength, &headersbuf, &headersLength);
 	res = send(clientfd, headersbuf, headersLength, 0);
@@ -161,8 +163,57 @@ int buildSocket(unsigned short port)
 	return sockfd;
 }
 
-int getFileName(const char *request, char **filename) 
+// request is null terminated within BUFSIZ
+// edits request to null terminate filename
+// and returns pointer to where filename starts
+int getFileName(char *request, char **filename) 
 {
+	unsigned int i;
+	char *method = "GET /";
+	unsigned int methodlen = strlen(method);
+	if (strlen(request) < methodlen) {
+		return 1;
+	}
+	for (i = 0; i < methodlen; i++) {
+		if (request[i] != method[i]) {
+			return 1;
+		}
+	}
+	// now we are pointing at char after /
+	// could be null
+	*filename = &(request[i]);
+	// turn first space into null:
+	while (request[i] != ' ' && request[i] != 0) { i++; }
+	request[i] = 0;
+	/*
+	for (; request[i] != 0; i++) {
+		if (request[i] == ' ') { 
+			request[i] = 0; 
+			break; 
+		}
+	}*/
+	
+	if (filename[0] == 0) {
+		strncpy(*filename, "index.html", BUFSIZ-methodlen); // check this isn't 1 byte overflow
+	}
+	
+	/*
+	int nitems = sscanf(request, "GET %""499s", request1);
+    if (nitems != 1)
+    {
+    	//ferror()
+    	perror("Error sscanf");
+        return 1;
+    }
+
+    // stupid scanf skips leading whitespaces
+    // so if no arg, I just get "HTTP......"
+    // so I'm including the / and if there is an arg it will be
+    // /word,
+    // otherwise "/\0"
+    *filename = &(request1[1]);
+    */
+    
 	return 0;
 }
 
@@ -181,11 +232,17 @@ int loadHTTPHeaders(off_t fileLength, char **headersbuf, unsigned long *headersL
 	return 0;
 }
 
-int fileNotFound() 
+int fileNotFound(int clientfd) 
 {
+	char *response = "404";
+	int res = send(clientfd, response, strlen(response), 0);
+	if (res != strlen(response)) {
+		perror("Error send file");
+		return 1;
+	}
 	return 0;
 }
-int fileNotAvailable() 
+int fileNotAvailable(int clientfd) 
 {
 	return 0;
 }
